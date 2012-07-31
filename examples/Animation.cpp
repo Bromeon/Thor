139c074d96052b0865af96471a70bb9f83e113bf
Jan Haller <bromeon@gmail.com>
2012-07-31 15:10:30 +0200

    [Examples] Animation: Created more complex image to animate

--- examples/Animation.cpp
+++ examples/Animation.cpp
@@ -2,17 +2,31 @@
 #include <Thor/Animation.hpp>
 #include <SFML/Graphics.hpp>
 
+// Adds a range of frames, assuming they are aligned as rectangles in the texture.
+// animation:      FrameAnimation to modify
+// x:              Column index of the texture rectangle
+// [yFirst,yLast]: Bounds for row indices (if yLast < yFirst, add frames in reverse order)
+// duration:       Relative duration of current frame (1 by default)
+void addFrames(thor::FrameAnimation& animation, int x, int yFirst, int yLast, float duration = 1.f)
+{
+	const int step = (yFirst < yLast) ? +1 : -1;
+	yLast += step; // so yLast is excluded in the range
+
+	for (int y = yFirst; y != yLast; y += step)
+		animation.addFrame(duration, sf::IntRect(36*x, 39*y, 36, 39));
+}
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(300, 200), "Thor Animation");
-	window.setFramerateLimit(20);
+	window.setVerticalSyncEnabled(true);
 	window.setKeyRepeatEnabled(false);
 
 	// Instruction text
 	sf::Text instructions(
-		"D:  Play drive animation (loop)\n"
-		"F:  Play fire animation\n"
-		"S:  Stop current animation\n"
+		"W:     Play walk animation (loop)\n"
+		"A:      Play attack animation\n"
+		"S:      Stop current animation\n"
 		"Esc:  Quit");
 	instructions.setCharacterSize(12);
 	instructions.setColor(sf::Color::White);
@@ -28,29 +42,33 @@ int main()
 	if (!texture.loadFromImage(image))
 		return 1;
 
-	// Create sprite and object that animates it
+	// Create sprite which is animated
 	sf::Sprite sprite(texture);
 	sprite.setPosition(100.f, 100.f);
-	thor::Animator<sf::Sprite, std::string> animator;
 
-	// Specify single subrect which is shown unless an other animation is active
+	// Define walk animation
+	thor::FrameAnimation walk;
+	addFrames(walk, 0, 0, 7);			// Frames 0..7	Right leg moves forward
+	addFrames(walk, 0, 6, 0);			// Frames 6..0	Right leg moves backward
+
+	// Define attack animation
+	thor::FrameAnimation attack;
+	addFrames(attack, 1, 0, 3);			// Frames 0..3	Lift gun
+	addFrames(attack, 1, 4, 4, 5.f);	// Frame  4		Aim (5 times normal frame duration)
+	for (int i = 0; i < 3; ++i)
+		addFrames(attack, 1, 5, 7);		// Frame  5..7	Fire (repeat 3 times)
+	addFrames(attack, 1, 4, 4, 5.f);	// Frame  4		Wait
+	addFrames(attack, 1, 3, 0);			// Frame  3..1	Lower gun
+
+	// Define static frame for stand animation
 	thor::FrameAnimation stand;
-	stand.addFrame(1.f, sf::IntRect(0, 21, 44, 21));
-
-	// Create first animation: Drive
-	thor::FrameAnimation drive;
-	for (unsigned int i = 0; i < 3; ++i)
-		drive.addFrame(1.f, sf::IntRect(0, 21*i, 44, 21));
-
-	// Create second animation: Fire
-	thor::FrameAnimation fire;
-	for (unsigned int i = 0; i < 4; ++i)
-		fire.addFrame(1.f, sf::IntRect(44, 21*i, 49, 21));
+	addFrames(stand, 0, 0, 0);
 
 	// Register animations with their corresponding durations
+	thor::Animator<sf::Sprite, std::string> animator;
+	animator.addAnimation("walk", walk, sf::seconds(1.f));
 	animator.addAnimation("stand", stand, sf::seconds(1.f));
-	animator.addAnimation("drive", drive, sf::seconds(0.4f));
-	animator.addAnimation("fire", fire, sf::seconds(0.3f));
+	animator.addAnimation("attack", attack, sf::seconds(1.f));
 
 	// Create clock to measure frame time
 	sf::Clock frameClock;
@@ -66,8 +84,8 @@ int main()
 			{
 				switch (event.key.code)
 				{
-					case sf::Keyboard::D:		animator.playAnimation("drive", true);			break;
-					case sf::Keyboard::F:		animator.playAnimation("fire");					break;
+					case sf::Keyboard::W:		animator.playAnimation("walk", true);			break;
+					case sf::Keyboard::A:		animator.playAnimation("attack");				break;
 					case sf::Keyboard::S:		animator.stopAnimation();						break;
 					case sf::Keyboard::Escape:	return 0;
 				}
