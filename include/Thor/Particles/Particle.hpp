50fc6e4ef64354920f0a1ec3e6dd5aca2d5308e3
Furkan Üzümcü <furkanuzumcu@gmail.com>
2014-08-03 19:56:05 +0300

    Correct an sf::Time divison problem
    There is no operator/ function for sf::Time, so have the functions
    divide the time as seconds.

--- include/Thor/Particles/Particle.hpp
+++ include/Thor/Particles/Particle.hpp
@@ -2,23 +2,23 @@
 //
 // Thor C++ Library
 // Copyright (c) 2011-2014 Jan Haller
-// 
+//
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
 // arising from the use of this software.
-// 
+//
 // Permission is granted to anyone to use this software for any purpose,
 // including commercial applications, and to alter it and redistribute it
 // freely, subject to the following restrictions:
-// 
+//
 // 1. The origin of this software must not be misrepresented; you must not
 //    claim that you wrote the original software. If you use this software
 //    in a product, an acknowledgment in the product documentation would be
 //    appreciated but is not required.
-// 
+//
 // 2. Altered source versions must be plainly marked as such, and must not be
 //    misrepresented as being the original software.
-// 
+//
 // 3. This notice may not be removed or altered from any source distribution.
 //
 /////////////////////////////////////////////////////////////////////////////////
@@ -48,40 +48,40 @@ namespace thor
 /// @n You normally don't have to work directly with particles outside of an affector or emitter.
 class THOR_API Particle
 {
-	// ---------------------------------------------------------------------------------------------------------------------------
-	// Public member functions
-	public:
-		/// @brief Constructor
-		/// @param totalLifetime How long the particle totally exists.
-		explicit					Particle(sf::Time totalLifetime);
+    // ---------------------------------------------------------------------------------------------------------------------------
+    // Public member functions
+    public:
+        /// @brief Constructor
+        /// @param totalLifetime How long the particle totally exists.
+        explicit					Particle(sf::Time totalLifetime);
 
 
-	// ---------------------------------------------------------------------------------------------------------------------------
-	// Public variables
-	public:
-		sf::Vector2f				position;			///< Current position.
-		sf::Vector2f				velocity;			///< Velocity (change in position per second).
-		float						rotation;			///< Current rotation angle.
-		float						rotationSpeed;		///< Angular velocity (change in rotation per second).
-		sf::Vector2f				scale;				///< Scale, where (1,1) represents the original size.
-		sf::Color					color;				///< %Particle color.
-		unsigned int				textureIndex;		///< Index of the used texture rect, returned by ParticleSystem::addTextureRect()
+    // ---------------------------------------------------------------------------------------------------------------------------
+    // Public variables
+    public:
+        sf::Vector2f				position;			///< Current position.
+        sf::Vector2f				velocity;			///< Velocity (change in position per second).
+        float						rotation;			///< Current rotation angle.
+        float						rotationSpeed;		///< Angular velocity (change in rotation per second).
+        sf::Vector2f				scale;				///< Scale, where (1,1) represents the original size.
+        sf::Color					color;				///< %Particle color.
+        unsigned int				textureIndex;		///< Index of the used texture rect, returned by ParticleSystem::addTextureRect()
 
 
-	// ---------------------------------------------------------------------------------------------------------------------------
-	// Private variables
-	private:
-		sf::Time					passedLifetime;		// Time passed since emitted.
-		sf::Time					totalLifetime;		// Total time to live.
+    // ---------------------------------------------------------------------------------------------------------------------------
+    // Private variables
+    private:
+        sf::Time					passedLifetime;		// Time passed since emitted.
+        sf::Time					totalLifetime;		// Total time to live.
 
 
-	// ---------------------------------------------------------------------------------------------------------------------------
-	// Friends
-	/// @cond FriendsAreAnImplementationDetail
-	friend class ParticleSystem;
-	friend sf::Time THOR_API getElapsedLifetime(const Particle& particle);
-	friend sf::Time THOR_API getTotalLifetime(const Particle& particle);
-	/// @endcond
+    // ---------------------------------------------------------------------------------------------------------------------------
+    // Friends
+    /// @cond FriendsAreAnImplementationDetail
+    friend class ParticleSystem;
+    friend sf::Time THOR_API getElapsedLifetime(const Particle& particle);
+    friend sf::Time THOR_API getTotalLifetime(const Particle& particle);
+    /// @endcond
 };
 
 /// @relates Particle
@@ -97,11 +97,11 @@ sf::Time THOR_API			getTotalLifetime(const Particle& particle);
 sf::Time THOR_API			getRemainingLifetime(const Particle& particle);
 
 /// @relates Particle
-/// @brief Returns <b>elapsed lifetime / total lifetime</b>.
+/// @brief Returns <b>elapsed lifetime / total lifetime</b> in seconds.
 float THOR_API				getElapsedRatio(const Particle& particle);
 
 /// @relates Particle
-/// @brief Returns <b>remaining lifetime / total lifetime</b>.
+/// @brief Returns <b>remaining lifetime / total lifetime</b> in seconds.
 float THOR_API				getRemainingRatio(const Particle& particle);
 
 /// @}
