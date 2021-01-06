f9b78905374353d7f2650cf404bfe9d5fe0a823d
Jan Haller <bromeon@gmail.com>
2021-01-06 15:56:20 +0100

    [All] 2021

--- doc/Documentation.hpp
+++ doc/Documentation.hpp
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 //
 // Thor C++ Library
-// Copyright (c) 2011-2019 Jan Haller
+// Copyright (c) 2011-2021 Jan Haller
 // 
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
